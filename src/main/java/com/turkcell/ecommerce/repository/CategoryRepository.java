e70c022ed9b98516a9be2f5c59c3c6acb209fadc
Umut <umut.gu@outlook.com>
2025-01-20 03:05:20 +0300

    Repositories

---
+++ src/main/java/com/turkcell/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.turkcell.ecommerce.repository;
+
+import com.turkcell.ecommerce.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
