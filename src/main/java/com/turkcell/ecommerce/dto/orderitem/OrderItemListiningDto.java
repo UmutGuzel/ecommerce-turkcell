4c32807d0f7926380657b7c6b9bf00f0a4501615
sedat <sedatalizevit@gmail.com>
2025-01-22 16:05:38 +0300

    add new Order methods, services and endpoints

--- src/main/java/com/turkcell/ecommerce/dto/orderitem/OrderItemListiningDto.java
+++ src/main/java/com/turkcell/ecommerce/dto/orderitem/OrderItemListiningDto.java
@@ -1,29 +1,18 @@
 package com.turkcell.ecommerce.dto.orderitem;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class OrderItemListiningDto {
     private UUID id;
     private String name;
 
     public OrderItemListiningDto(UUID id, String name) {
         this.id = id;
-        this.name = name;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 }
--- src/main/java/com/turkcell/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/turkcell/ecommerce/service/OrderServiceImpl.java
@@ -1,9 +1,16 @@
 package com.turkcell.ecommerce.service;
 
+import com.turkcell.ecommerce.entity.Cart;
+import com.turkcell.ecommerce.entity.CartItem;
 import com.turkcell.ecommerce.entity.Order;
+import com.turkcell.ecommerce.entity.OrderItem;
+import com.turkcell.ecommerce.repository.OrderItemRepository;
 import com.turkcell.ecommerce.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,9 +18,11 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
         this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
     }
 
 
@@ -22,5 +31,33 @@ public class OrderServiceImpl implements OrderService {
         return orderRepository.findById(id);
     }
 
+    @Override
+    public void createOrder(Cart cart) {
+        if (cart == null){
+            throw new RuntimeException("Sepet bulunamadı");
+        }
+        if(cart.getCartItems() == null || cart.getCartItems().isEmpty()){
+            throw new RuntimeException("Sepet boş olamaz");
+        }
+
+        Order order = new Order();
+        List<CartItem> cartItems = cart.getCartItems();
+
+        cartItems.stream()
+                .map(cartItem -> {
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setProduct(cartItem.getProduct());
+                    orderItem.setQuantity(cartItem.getQuantity());
+                    orderItem.setPrice(cartItem.getProduct().getPrice().multiply(BigDecimal.valueOf(cartItem.getQuantity())));
+                    orderItem.setOrder(order);
+                    orderItem.setStatus("Hazırlanıyor");
+                    return orderItem;
+                })
+                .forEach(orderItemRepository::save);
+
+        order.setCreatedAt(new Date(System.currentTimeMillis()));
+        orderRepository.save(order);
+
+    }
 
 }
